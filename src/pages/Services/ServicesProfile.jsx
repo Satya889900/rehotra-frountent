5676663279990dea68f0296d20239bdd1b4a100a
SP889900 <satya.prakash.fiem.cse20@teamfuture.in>
2025-10-27 14:50:18 +0530

    fifthCommit

--- src/pages/Services/ServicesProfile.jsx
+++ src/pages/Services/ServicesProfile.jsx
@@ -236,7 +236,7 @@ export default function Services(){
               <div className="flex items-start">
                 <svg className="w-4 h-4 mr-2 mt-0.5 text-gray-400 shrink-0" fill="none" stroke="currentColor" viewBox="0 0 24 24">
                   <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M19 20H5a2 2 0 01-2-2V6a2 2 0 012-2h10a2 2 0 012 2v1m2 13a2 2 0 01-2-2V7m2 13a2 2 0 002-2V9a2 2 0 00-2-2h-2m-4-3H9M7 16h6M7 8h6v4H7V8z" />
-                </svg>
+                </svg> 
                 <div className="min-w-0 flex-1">
                   <div className="font-medium text-gray-900 mb-0.5">Business License</div>
                   <div className="truncate">({profile.electricalLicense})</div>
